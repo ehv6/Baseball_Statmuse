e9c80202ed9d8cef1aaafeea0c2919983bf37af7
Tushig Battulga <ehv6@txstate.edu>
2025-03-23 22:24:20 -0500

    file paths

--- find_hr_leader.py
+++ find_hr_leader.py
@@ -1,9 +1,13 @@
 import sqlite3
 import pandas as pd
 
+# Define datapath
+filepath_batting = "/Users/tushigbattulga/Downloads/2024csvs_files/2024batting.csv"
+filepath_players = "/Users/tushigbattulga/Downloads/2024csvs_files/2024allplayers.csv"
+
 # Load CSV data into DataFrames
-batting_df = pd.read_csv("2024batting.csv")
-players_df = pd.read_csv("2024allplayers.csv")
+batting_df = pd.read_csv(filepath_batting)
+players_df = pd.read_csv(filepath_players)
 
 # Create SQLite database
 conn = sqlite3.connect("baseball.db")
